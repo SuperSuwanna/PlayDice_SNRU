a6d49b4d01060904c502f63597a1c86d818948a3
suwanna <student05@Student05.local>
2019-03-18 15:51:25 +0700

    Initial Commit

---
+++ PlayDice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayDice
+//
+//  Created by Student05 on 18/3/2562 BE.
+//  Copyright © 2562 Suwanna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
